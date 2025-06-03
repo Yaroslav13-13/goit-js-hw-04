2b2c51da57b1ec67e6de6518e554bfad81987dc0
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-03 16:15:05 +0300

    HW-JS-04

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,13 @@ const profile = {
   username: "Jacob",
   playTime: 300,
   changeUsername(newName) {
-    profile.username = newName;
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
   },
 };
 
